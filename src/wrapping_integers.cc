9b5f69591c5a4900203fb86c7a1f019071ff645b
AKmoex <akmoex@gmail.com>
2023-05-19 14:54:58 +0800

    checkpoint2-wrap & unwrap

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -1,19 +1,23 @@
 #include "wrapping_integers.hh"
-
+#include <iostream>
 using namespace std;
+
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+
+  uint32_t seqno = (uint32_t)((n + zero_point.raw_value_) % ((uint64_t)1<<32));
+
+  return Wrap32 { seqno };
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  uint64_t offset = raw_value_ - zero_point.raw_value_;
+  if(offset < checkpoint){
+    cout<<offset<<endl;
+    uint64_t mod_num = (checkpoint - offset + ((uint64_t)1<<31)) / ((uint64_t)1<<32);
+    offset += mod_num*((uint64_t)1<<32);
+  }
+  return offset;
 }
